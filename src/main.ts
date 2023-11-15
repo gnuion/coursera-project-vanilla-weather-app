e89a2971e25ce6241ae953125de49af46876888e
Jon <jon@example.com>
2023-11-15 19:27:01 +0100

    Add Fetch logic for the 7Timer! API and unit conversion

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,27 @@ type City = {
   country: string;
 }
 
+type WeatherData = {
+  date: number,
+  temp2m: {
+    max: number,
+    min: number
+  }
+  weather: string,
+  wind10m_max: number
+}
+
+type Unit = 'fahrenheit' | 'celsius'
+
+let dataseries: WeatherData[];
+let unit: Unit = 'celsius'
+const cardsEl = document.querySelector<HTMLDivElement>('#cards')!
+const spinner = document.querySelector<HTMLDivElement>('.lds-spinner')!
+const toggleUnitEl = document.querySelector("#toggle-unit")!
+
+
+
+// #region populate cities in options
 const populateCities = () => {
   return fetch("/city_coordinates.csv")
     .then(response => response.text())
@@ -31,12 +52,130 @@ const populateCities = () => {
     })
 }
 const cities = await populateCities();
-const citySelect = document.querySelector<HTMLDivElement>('#city-select')!
+const citySelect = document.querySelector<HTMLDivElement>('#city-select select')!
 for (let [key, value] of cities) {
   const optionEl = document.createElement('option')
   optionEl.value = String(key)
-  optionEl.text = value.city
+  optionEl.text = value.city + ', ' + value.country
   citySelect.appendChild(optionEl)
 }
+// #endregion
 
-// document.querySelector<HTMLDivElement>('#app')!.innerHTML = "hello"
+
+
+const fetchWeather = async (longitude: string, latitude: string) => {
+  const url = `https://www.7timer.info/bin/civillight.php?lon=${longitude}&lat=${latitude}&ac=0&unit=metric&output=json&tzshift=0"`
+  const dataseries = await fetch(url).then(data => data.json()).then(json => {
+    return json.dataseries as WeatherData[]
+  })
+  return dataseries
+}
+
+citySelect.addEventListener('change', async (event) => {
+  const key = parseInt((event.target as HTMLSelectElement).value)
+  if (!key) return;
+  let city = cities.get(key);
+  if (!city) return;
+  spinner.style.display = 'block'
+  cardsEl.style.display = 'none'
+  dataseries = await fetchWeather(city.longitude, city.latitude)
+  repopulateCards()
+  spinner.style.display = 'none'
+  cardsEl.style.display = 'flex'
+})
+
+const repopulateCards = () => {
+  cardsEl.innerHTML = '';
+  if (!dataseries) { return }
+  dataseries.forEach(data => {
+    let max, min;
+    const { weather, temp2m } = data
+    if (unit == 'fahrenheit') {
+      max = (temp2m.max * 9 / 5) + 32
+      min = (temp2m.min * 9 / 5) + 32
+    } else {
+      max = temp2m.max
+      min = temp2m.min
+    }
+    const date = formatDate(String(data.date))
+    const template = `
+    <div class="card">
+      <p>${date}</p>
+      <div style="overflow:hidden; width:64px;">
+        <img src="/images/${weather}.png"/>
+      </div>
+      <p>${weather}</p>
+      <p>High: ${max} ${units[unit].symbol}</p>
+      <p>Low: ${min} ${units[unit].symbol}</p>
+    </div>`
+    const cardEl = document.createElement('div')
+    cardEl.innerHTML = template
+    cardsEl.appendChild(cardEl)
+  })
+}
+
+const formatDate = (inputDateString: string) => {
+  // Parse the input string into a Date object
+  const date = new Date(
+    parseInt(inputDateString.substring(0, 4)), // Year
+    parseInt(inputDateString.substring(4, 6), 10) - 1, // Month (zero-based)
+    parseInt(inputDateString.substring(6, 8)) // Day
+  );
+  // Array of month names
+  const monthNames = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+  // Array of day names
+  const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+  // Get day, month, and year components
+  const dayName = dayNames[date.getDay()].substring(0, 3);
+  const monthName = monthNames[date.getMonth()].substring(0, 3);
+  const year = date.getFullYear();
+
+  // Format the output
+  const formattedDate = `${dayName} ${monthName} ${year}`;
+
+  return formattedDate;
+}
+
+let units = {
+  'fahrenheit': {
+    label: 'Fahrenheit',
+    symbol: 'ºF'
+  },
+  'celsius': {
+    label: 'Celcius',
+    symbol: 'ºC'
+  },
+}
+
+const toggleUnit = () => {
+  if (unit == 'celsius') {
+    unit = 'fahrenheit'
+  } else {
+    unit = 'celsius'
+  }
+  renderUnit()
+  repopulateCards()
+}
+
+const renderUnit = () => {
+  let first = toggleUnitEl.children[0]
+  let last = toggleUnitEl.children[1]
+  if (unit == 'celsius') {
+    first.textContent = `Using ${units.celsius.label}.`
+    last.textContent = `Switch to ${units.fahrenheit.symbol}`
+  } else {
+    first.textContent = `Using ${units.fahrenheit.label}.`
+    last.textContent = `Switch to ${units.celsius.symbol}`
+  }
+}
+
+toggleUnitEl.innerHTML = `
+<span></span>
+<span class="toggler"></span>
+`
+toggleUnitEl.children[1].addEventListener('click', toggleUnit)
+toggleUnit()
